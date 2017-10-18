18e19e1af88a8a0043e5745679e8d3d16ce645b4
Ruslan Salikhov <ruslan.salihov@gmail.com>
2017-10-18 23:26:29 +0500

    karma

--- bitsharesbase/chains.py
+++ bitsharesbase/chains.py
@@ -2,5 +2,9 @@ known_chains = {
     "KRM": {
         "chain_id": "3ac412e061ebbf16f483751dbe17643b690bd6fe58db826032d7bf57fd5acb38",
         "core_symbol": "KRM",
-        "prefix": "KRM"}
+        "prefix": "KRM"},
+    "BTS": {
+        "chain_id": "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8",
+        "core_symbol": "BTS",
+        "prefix": "BTS"}
 }
