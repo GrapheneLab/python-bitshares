b1286a3a0d82059cc62eb60032bebd853a1334f2
Ruslan Salikhov <ruslan.salihov@gmail.com>
2017-11-26 14:42:00 +0500

    update

--- bitsharesbase/chains.py
+++ bitsharesbase/chains.py
@@ -1,6 +1,6 @@
 known_chains = {
     "KRM": {
-        "chain_id": "32f0eb325b85ec5ad49f9139e52e1b71fe2035bb8cdbc3c7d98a77238460e8fb",
+        "chain_id": "70574380ae65d6b30f71a375bf205ebbb0d852bf877a6c62fdcd03cbd1bfcd83",
         "core_symbol": "KRM",
         "prefix": "KRM"},
     "BTS": {
