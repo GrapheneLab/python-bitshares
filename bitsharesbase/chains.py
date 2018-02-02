7814869e5d693739f4cce456e1a951a0f5800dbc
Ruslan Salikhov <ruslan.salihov@gmail.com>
2018-02-02 16:08:22 +0500

    Added Karma Testnet

--- bitsharesbase/chains.py
+++ bitsharesbase/chains.py
@@ -3,6 +3,10 @@ known_chains = {
         "chain_id": "c85b4a30545e09c01aaa7943be89e9785481c1e7bd5ee7d176cb2b3d8dd71a70",
         "core_symbol": "KRM",
         "prefix": "KRM"},
+    "KRMT": {
+        "chain_id": "e81bea67cebfe8612010fc7c26702bce10dc53f05c57ee6d5b720bbe62e51bef",
+        "core_symbol": "KRMT",
+        "prefix": "KRMT"},
     "BTS": {
         "chain_id": "4018d7844c78f6a6c41c6a552b898022310fc5dec06da467ee7905a8dad512c8",
         "core_symbol": "BTS",
